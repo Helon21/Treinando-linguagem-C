65a8832786f1ea2b4cdd418eb523ba3316c35030
Helon21 <helonbentes51@hotmail.com>
2022-11-04 17:12:51 -0300

    segundo dia de treino no beecrowd

---
+++ beecrowd/exemplo python.py
@@ -0,0 +1,8 @@
+num = int(input("Informe um número inteiro"))
+
+if(num % 2 == 0):
+    print("Numero par", num)
+elif(num % 2 != 0):
+    print("Numero impar", num)
+else:
+    print("Digite somente um número inteiro")
